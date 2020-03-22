b62c5b326abebb0ad7d367c49bdc4761399ea651
4dnkgg <61512793+4dnkcc@users.noreply.github.com>
2020-03-22 18:12:48 +0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { prefix, token, } = require('./config.json');
+const { prefix, } = require('./config.json');
 const config = require('./config.json');
 const client = new Discord.Client();
 const { accounts, } = require('./accounts.json');
